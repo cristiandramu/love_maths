e78c1c48b840ff6e2b69473dc7c4a5b73be59a67
cristiandramu <christian.dramu@gmail.com>
2022-12-30 00:03:53 +0000

    added code for the subtract game

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,8 +31,14 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+
     } else if (gameType === "multiply"){
         displayMultiplyQuestion(num1, num2);
+
+    } else if (gameType === "subtract"){
+        displaySubtractQuestion(num1, num2);
+        
+
     }
 
     else {
@@ -76,10 +82,15 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+
     } else if(operator === "x"){
         return [operand1 * operand2, "multiply"];
 
-    } else {
+    } 
+    else if(operator === "-"){
+        return [operand1 - operand2, "subtract"]
+    }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -112,12 +123,16 @@ function displayAdditionQuestion(operand1, operand2) {
     
 }
 
-function displaySubtractQuestion() {
-  
+function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
+
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
