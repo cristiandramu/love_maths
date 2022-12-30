f716b69fc5f2af2e9eb4f51f863dd28313858dd1
cristiandramu <christian.dramu@gmail.com>
2022-12-30 00:42:10 +0000

    Added code for devision game

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,8 +47,9 @@ function runGame(gameType) {
 
     } else if (gameType === "subtract"){
         displaySubtractQuestion(num1, num2);
-
-
+    }
+    else if (gameType === "division"){
+        displayDivisionQuestion(num1, num2);
     }
 
     else {
@@ -100,6 +101,9 @@ function calculateCorrectAnswer() {
     else if(operator === "-"){
         return [operand1 - operand2, "subtract"]
     }
+    else if(operator === "/"){
+        return [operand1 / operand2, "division"]
+    }
     else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -146,5 +150,14 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+    
+}
+function displayDivisionQuestion(operand1, operand2) {
+    operand1= operand1 * operand2;
+
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+
     
 }
